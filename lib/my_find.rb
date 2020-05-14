af172bcd37111da5e0040efbda0218a8183657d9
Terren Culberson <culbersonterren@yahoo.com>
2020-05-14 04:28:37 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -3,10 +3,8 @@ require 'pry'
 def my_find(collection)
   i = 0 
     while i < collection.length 
-   return collection[i]  yield(collection[i])
+   return collection[i] if yield(collection[i])
     i =+ l 
   end 
 end
 
-collection = (1..100).to_a 
-my_find(collection) {|i| i % 3 == 0 and i % 5 == 0 } 
